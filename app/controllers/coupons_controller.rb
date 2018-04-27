22140f5f4d6eb8696bff3ca783c437141a96daa0
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-04-27 00:16:38 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -3,7 +3,7 @@ class CouponsController < ApplicationController
   def index
      @coupons = Coupon.all
   end
-  
+
   def create
     #binding.pry
     @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
