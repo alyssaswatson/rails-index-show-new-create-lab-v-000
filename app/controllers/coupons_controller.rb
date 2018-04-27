6889379329cf08b7b59ffd51a4b60f58efc60908
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-04-27 00:09:01 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -2,8 +2,9 @@ class CouponsController < ApplicationController
 
 
   def create
+    binding.pry
     @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
     redirect_to coupon_path(@coupon)
   end
-  
+
 end
