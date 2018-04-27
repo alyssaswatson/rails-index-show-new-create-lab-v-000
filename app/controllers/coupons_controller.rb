657922c2b913f40d05dc669c6c0e89789dcf5872
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-04-27 00:01:45 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -2,6 +2,7 @@ class CouponsController < ApplicationController
 
 
   def create
-
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    redirect_to student_path(@student)
   end
 end
